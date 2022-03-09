3a39554ab1eecc6f41be7fbfcd4a9d03ec74d586
Mayan <gabrielmayan@gmail.com>
2022-03-09 20:20:31 -0300

    Correção de bugs e task de finalização de tarefa

--- src/controllers/task.js
+++ src/controllers/task.js
@@ -1,11 +1,11 @@
 const { addTime } = require('../helpers/handleDate');
-const { insertInfo } = require('../helpers/handleKnex');
 const { generateUuid } = require('../helpers/handleUuid');
+const { insertInfo, findOneBy, updateInfo } = require('../helpers/handleKnex');
 
-const createTask = async (requst, response) => {
+const createTask = async (request, response) => {
 	try {
-		const { user } = requst;
-		const { description, deadline } = requst.body;
+		const { user } = request;
+		const { description, deadline } = request.body;
 
 		const newTask = {
 			id: generateUuid(description),
@@ -25,4 +25,33 @@ const createTask = async (requst, response) => {
 	}
 }
 
-module.exports = { createTask };
+const finalizeTask = async (request, response) => {
+	try {
+		const { id } = request.params;
+		const { id: userId } = request.user;
+		const conditions = { id, userId };
+
+		const task = await findOneBy('task', conditions)
+
+		if (!task)
+			return response.status(200).json('Tarefa inexistente ou não partence ao usuario');
+
+		if (task.completedAt)
+			return response.status(200).json('Tarefa já concluída');
+
+		if (task.deletedAt)
+			return response.status(200).json('Não é possível completar tarefas excluidas');
+
+		const now = new Date();
+		const taskCompleted = await updateInfo('task', conditions, { updatedAt: now, completedAt: now })
+
+		if (!taskCompleted)
+			return response.status(200).json('Falha ao atualizar a tarefa');
+
+		return response.status(200).json(taskCompleted);
+	} catch (error) {
+		return response.status(400).json('Falha ao atualizar a tarefa')
+	}
+}
+
+module.exports = { createTask, finalizeTask };
--- src/routes.js
+++ src/routes.js
@@ -1,21 +1,23 @@
 const { Router } = require('express');
 const { login } = require('./controllers/login');
 const { createUser } = require('./controllers/user');
-const { createTask } = require('./controllers/task');
+const { createTask, finalizeTask } = require('./controllers/task');
 
 const { loginSchema } = require('./validations/loginSchema');
-const { createUserSchema, createTaskSchema } = require('./validations/userSchema');
+const { createUserSchema } = require('./validations/userSchema');
+const { createTaskSchema, finalizeTaskSchema } = require('./validations/taskSchema')
 
 const authentication = require('./middlewares/authentication');
-const { validateBody } = require('./middlewares/validateRequest');
+const { validateBody, validateParams } = require('./middlewares/validateRequest');
 
 const routes = Router();
 
 routes.get('/', (_, response) => response.status(200).json({ message: 'Teste OK' }));
 
 routes.post('/login', validateBody(loginSchema), login);
-routes.post('/create-user', validateBody(createUserSchema), createUser);
+routes.post('/user/create', validateBody(createUserSchema), createUser);
 
-routes.post('/create-task', authentication, validateBody(createTaskSchema), createTask);
+routes.post('/task/finalize/:id', authentication, validateParams(finalizeTaskSchema), finalizeTask);
+routes.post('/task/create', authentication, validateBody(createTaskSchema), createTask);
 
 module.exports = routes;
