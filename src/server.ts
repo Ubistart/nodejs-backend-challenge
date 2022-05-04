feb09b8d24a5e578377a8e610e4aaaa330525da6
bellacbs <bellacbrandao@gmail.com>
2022-05-04 15:22:53 -0300

    :checkered_flag: iniciando projeto

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from 'cors'
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use(cors())
+
+
+
+module.exports = app
